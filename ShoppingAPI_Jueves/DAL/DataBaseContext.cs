58f432e3cb8bd3a9b022bb483c24308be5b0766c
CamiloUB <126737141+CamiloUB@users.noreply.github.com>
2024-02-23 21:11:51 -0500

    Commit, before migration

---
+++ ShoppingAPI_Jueves/DAL/DataBaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingAPI_Jueves.DAL.Entities;
+
+namespace ShoppingAPI_Jueves.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)       
+        {
+         
+        }
+
+        #region DbSets
+
+        public DbSet<Country> Countries { get; set; }
+        // public DbSet<City> Cities { get; set; }
+        // public DbSet<User> Users { get; set; }
+
+
+
+
+        #endregion
+    }
+}
--- ShoppingAPI_Jueves/DAL/Entities/Country.cs
+++ ShoppingAPI_Jueves/DAL/Entities/Country.cs
@@ -1,8 +1,13 @@
-﻿namespace ShoppingAPI_Jueves.DAL.Entities
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingAPI_Jueves.DAL.Entities
 {
     public class Country : AuditBase
     {
-
+        [Display(Name = "País")]
+        // [MaxLenght(50, ErrorMessage = "El campo{0} debe tener máximo {1} caracteres. ")]
+        [Required]
         public string Name { get; set; }
     }
 }
